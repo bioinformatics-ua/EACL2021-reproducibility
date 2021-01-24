cf3f1b3a6e92b8826388a22b6ad0b1be0114d816
Tiago Almeida <tiagomeloalmeida@ua.pt>
2021-01-24 00:35:13 +0000

    update the url to download_folder

--- _other_dependencies/download_folder.sh
+++ _other_dependencies/download_folder.sh
@@ -2,7 +2,7 @@
 
 # script to download from google drive, source: https://stackoverflow.com/questions/48133080/how-to-download-a-google-drive-url-via-curl-or-wget/48133859
 
-fileid="18NQVsGJtFxRPMt_bw0TXgtUkyDPzol4V"
+fileid="1FPrg3-hIc0RPYvlTkXUZAaChfbHv4Vam"
 filename="download_folder.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
